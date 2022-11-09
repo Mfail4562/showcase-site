7c25d83b630446caf059b7be155ffa54b316d855
Mfail4562 <mathieu.perin.prog@gmail.com>
2022-11-09 10:27:21 +0100

    creation de la fonction selection aleatoire de la face

---
+++ rollingDice/dice.js
@@ -0,0 +1,5 @@
+function rollingDice(numberFaces) //selection aleatoire de la face en fonction du nombre de face
+{
+    let roll= 1+Math.round(Math.random()*numberFaces);
+    console.log(roll);
+}
