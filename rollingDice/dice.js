081d7251f2f419f5940b335f13db15a772f69bf5
Mfail4562 <mathieu.perin.prog@gmail.com>
2022-11-10 15:50:38 +0100

    modification du bouton 'roll the dice' et gestion des marge haute. ajout de la classe 'comming soon' et renvoie de lien depuis l'index html

--- rollingDice/dice.js
+++ rollingDice/dice.js
@@ -1,10 +1,32 @@
+
+
 document
 .getElementById("roll")
 .addEventListener('click', function (e) {
 
 e.preventDefault();
 e.stopPropagation();
-let roll= 1+Math.round(Math.random()*19);
+
+let dicetab = document.getElementsByName('dices')
+let valeur;
+let number;
+
+for (let i=0; i<dicetab.length; i++){
+    if(dicetab[i].checked){
+valeur = dicetab[i].value;
+    }
+}
+switch (valeur){
+    case '3' : number = 3;
+    break;
+    case '5' : number = 5;
+    break;
+    case '19' : number = 19;
+    break;
+    default : return 0;
+}
+
+let roll= 1+Math.round(Math.random()*number);
     document
     .getElementById("dicesResult")
     .innerText = (roll) + '' ;
