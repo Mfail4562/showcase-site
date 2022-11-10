1d3cdba4ce78b96de50d841fa4716d611af82135
Bloodysunset <chevalier.nicolas2103@gmail.com>
2022-11-10 19:46:02 +0100

    fix: cleaning some code

--- rollingDice/dice.js
+++ rollingDice/dice.js
@@ -1,33 +1,30 @@
+document.getElementById("roll").addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
 
+  let dicetab = document.getElementsByName("dices");
+  let valeur = 0;
+  let number;
 
-document
-.getElementById("roll")
-.addEventListener('click', function (e) {
-
-e.preventDefault();
-e.stopPropagation();
-
-let dicetab = document.getElementsByName('dices')
-let valeur;
-let number;
-
-for (let i=0; i<dicetab.length; i++){
-    if(dicetab[i].checked){
-valeur = dicetab[i].value;
+  for (let i = 0; i < dicetab.length; i++) {
+    if (dicetab[i].checked) {
+      valeur = dicetab[i].value;
     }
-}
-switch (valeur){
-    case '3' : number = 3;
-    break;
-    case '5' : number = 5;
-    break;
-    case '19' : number = 19;
-    break;
-    default : return 0;
-}
+  }
+  //   switch (valeur) {
+  //     case "3":
+  //       number = 3;
+  //       break;
+  //     case "5":
+  //       number = 5;
+  //       break;
+  //     case "19":
+  //       number = 19;
+  //       break;
+  //     default:
+  //       return 0;
+  //   }
 
-let roll= 1+Math.round(Math.random()*number);
-    document
-    .getElementById("dicesResult")
-    .innerText = (roll) + '' ;
+  let roll = 1 + Math.round(Math.random() * valeur);
+  document.getElementById("dicesResult").innerText = roll + "";
 });
